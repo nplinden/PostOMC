6df2ddf3a91f9411b3ac01caa14cf00207088854
Nicolas Linden <nicolas@nlinden.com>
2025-08-05 22:49:50 +0200

    Refactor CLI commands and improve code formatting for consistency

--- src/postomc/CLI/cli.py
+++ src/postomc/CLI/cli.py
@@ -3,10 +3,12 @@ from postomc.CLI.export import export
 from postomc.CLI.info import info
 from postomc.CLI.plot import plot
 
+
 @click.group()
 def cli():
     """Command line utility for post-processing OpenMC depletion results."""
     pass
+
 
 cli.add_command(export)
 cli.add_command(info)
